9bdd8c4a3f280bbc6772ae32833cd94693f674b1
Sambit <sambitpatra040@gmail.com>
2025-05-29 11:00:58 +0530

    LongestPalindromeSubstring

---
+++ LongestPalindromeSubstring.java
@@ -0,0 +1,32 @@
+public class LongestPalindromeSubstring {
+    public static String longestPalindrome(String s) {
+        if(s == null || s.length() < 1)
+            return "";
+
+        int start = 0, end = 0;
+
+        for(int i = 0; i < s.length(); i++){
+            int len1 = expandFromCentre(s, i, i);
+            int len2 = expandFromCentre(s, i, i+1);
+            int len = Math.max(len1, len2);
+
+            if(len > end-start){
+                start = i - (len-1) /2;
+                end = i + len/2;
+            }
+        }
+        return s.substring(start, end + 1);
+    }
+
+    private static int expandFromCentre(String s, int left, int right){
+        while(left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)){
+            left--;
+            right++;
+        }
+        return right - left -1;
+    }
+    public static void main(String[] args) {
+        String input = "babad";
+        System.out.println("Longest Palindromic Substring: " + longestPalindrome(input)); // Output: "bab" or "aba"
+    }
+}
